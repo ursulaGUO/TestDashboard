44bf90930b077e4ae3143ae195dab2ade00805d0
ursulaGUO <80830724+ursulaGUO@users.noreply.github.com>
2024-06-12 14:06:37 -0500

    Update DashboardExample.py

--- DashboardExample.py
+++ DashboardExample.py
@@ -13,15 +13,65 @@ st.set_page_config(
 
 alt.themes.enable("dark")
 
-df = pd.read_csv("VolvoAllUsedTrend.csv")
-df.drop(df.tail(1).index,inplace=True)
 
+###########################
+# Prepping the dataframe
+###########################
+df = pd.read_csv("VolvoAllUsedTrend.csv")
+
+# Dropping last line which contains irrelevant data
+df.drop(df.tail(1).index,inplace=True) 
+
+# Extracting year out of date
+df["Date"] = pd.to_datetime(df["Date"])
+df["Year"] = df["Date"].dt.year
+df_new = pd.DataFrame()
+
+# Creating two seperate columns with different prices
+df["Volvo Price"] = df["Price"].where(df["Car Type"] == "Volvo")
+df["Average Market Price"] = df["Price"].where(df["Car Type"] == "CarGurus Index")
+df_volvo = df.dropna(subset = ['Volvo Price'])
+df_volvo.drop(columns = ["Average Market Price"],inplace=True)
+df_market = df.dropna(subset = ["Average Market Price"])
+df_market.drop(columns = ["Volvo Price"],inplace=True)
+df = pd.merge(df_volvo, df_market[["Date","Average Market Price"]], on="Date")
+
+###########################
+# Sidebar selection options
+###########################
+'''
+with st.sidebar:
+    st.title('🚕 Volvo Price Dashboard')
+    year_list = list(df.Year.unique())[::-1]
+    selected_year = st.selectbox("Select Year", year_list, index=len(year_list)-1)
+    df_selected_year = df[df.Year == selected_year]
+    df_selected_year_sorted = df_selected_year.sort_values(by="Price", ascending=False)
+    color_theme_list = ['blues','cividis','greens']
+    select_color_theme = st.selectbox("Select a color theme", color_theme_list)
+'''
+
+###########################
+# Sidebar selection options
+###########################
+# Include a start date and an end date
 
 with st.sidebar:
     st.title('🚕 Volvo Price Dashboard')
-    date_list = list(df.Date.unique())[::-1]
-    selected_date = st.selectbox("Select Date", date_list, index=len(date_list)-1)
-    df_selected_date = df[df.Date == selected_date]
-    df_selected_date_sorted = df_selected_date.sort_values(by="Price", ascending=False)
+    year_list = list(df.Year.unique())[::]
+    selected_start_year = st.selectbox("Select Start Year", year_list, index=len(year_list)-1)
+    selected_end_year = st.selectbox("Select End Year", year_list, index=len(year_list)-1)
+    df_selected_years = df[(df.Year >= selected_start_year) & (df.Year <= selected_end_year)]
+    df_selected_years_sorted = df_selected_years.sort_values(by="Price", ascending=False)
     color_theme_list = ['blues','cividis','greens']
     select_color_theme = st.selectbox("Select a color theme", color_theme_list)
+
+
+# Writing paragraphs on page
+st.write("""
+         # Volvo Sales Trend
+         Click to see how Volvo's price has changed over time in the second hand market""")
+
+df_selected_years
+
+# Plotting a line chart on page
+st.line_chart(data=df_selected_years, x='Date', y=["Volvo Price", "Average Market Price"])
